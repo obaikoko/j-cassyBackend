26decf3b4789f7e46e4fba7176401e20b3b56634
obaikoko <jesseobinna7@gmail.com>
2023-10-21 22:16:38 +0100

    another commit

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const connectDB = require('./config/db');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 const colors = require('colors');
-const cors = require('cors')
+const cors = require('cors');
 const { errorHandler } = require('./middleware/errorMiddleware');
 
 connectDB();
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/products', require('./routes/productRoutes'));
 app.use(errorHandler);
