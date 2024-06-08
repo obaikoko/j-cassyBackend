32d525dfb75caf8b97da5a1a0ec6fdd48df2c125
obaikoko <jesseobinna7@gmail.com>
2024-06-08 13:57:16 +0100

    fixed bug

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-import path from 'path';
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
@@ -10,7 +9,6 @@ import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import userRoute from './routes/userRoutes.js';
 import productRoute from './routes/productRoutes.js';
 import orderRoute from './routes/orderRoutes.js';
-import uploadRoute from './routes/uploadRoutes.js';
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -32,27 +30,7 @@ app.use(express.json());
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/orders', orderRoute);
-app.get('/api/config/paypal', (req, res) =>
-  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
-);
 
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
-if (process.env.NODE_ENV === 'production') {
-  app.use('/uploads', express.static('/var/data/uploads'));
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
-
-app.use('/api/upload', uploadRoute);
 app.use(errorHandler);
 app.use(notFound);
 app.listen(port, () => console.log(`Server running on ${port}`));
