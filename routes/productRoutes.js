71816ca886c3d095ea81f7958e8f52c8332f1f4e
obaikoko <jesseobinna7@gmail.com>
2024-06-09 09:41:47 +0100

    fixing bugs

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,21 +3,21 @@ import {
   getProducts,
   getProduct,
   createProduct,
-  // updateProduct,
+  updateProduct,
   deleteProduct,
   createProductReview,
-  getTopProducts,
+  getTopProducts
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id/reviews').post(protect, createProductReview);
-router.route('/top').get(getTopProducts);
+router.route('/top').get( getTopProducts);
 router
   .route('/:id')
   .get(getProduct)
-  // .put(protect, admin, updateProduct)
+  .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
 
 export default router;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
@@ -9,6 +10,7 @@ import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import userRoute from './routes/userRoutes.js';
 import productRoute from './routes/productRoutes.js';
 import orderRoute from './routes/orderRoutes.js';
+import uploadRoute from './routes/uploadRoutes.js';
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -17,8 +19,6 @@ connectDB();
 const app = express();
 const corsOptions = {
   origin: 'http://localhost:3000',
-  // origin: 'https://jcassy.vercel.app',
-
   credentials: true,
 };
 
@@ -30,7 +30,27 @@ app.use(express.json());
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/orders', orderRoute);
+app.get('/api/config/paypal', (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
 
-app.use(notFound);
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
+
+app.use('/api/upload', uploadRoute);
 app.use(errorHandler);
+app.use(notFound);
 app.listen(port, () => console.log(`Server running on ${port}`));
